117d3fefa450f7d5074970d271d0f6fed461afb7
Lucas Almeida <dev2@case.inf.br>
2024-02-24 17:23:47 -0300

    Refactoring do controller de pessoas

--- src/main/java/br/com/lucas/controllers/PersonController.java
+++ src/main/java/br/com/lucas/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,17 +23,14 @@ public class PersonController {
 
 	@Autowired
 	private PersonServices service;
-	// PersonServices service = new PersonServices();
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Person> findByAll() {
-
 		return service.findAll();
 	}
 
 	@GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findById(@PathVariable(value = "id") Long id) {
-
 		return service.findById(id);
 	}
 
@@ -47,8 +45,8 @@ public class PersonController {
 	}
 
 	@DeleteMapping(value = "{id}")
-	public void delete(@PathVariable(value = "id") Long id) {
-
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
