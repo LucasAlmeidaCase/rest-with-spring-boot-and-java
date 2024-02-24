6823167e4724e322ad2648e025755d07b03dea0b
Lucas Almeida <dev2@case.inf.br>
2024-02-24 14:39:51 -0300

    Verbs

---
+++ src/main/java/br/com/lucas/controllers/PersonController.java
@@ -0,0 +1,54 @@
+package br.com.lucas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.lucas.model.Person;
+import br.com.lucas.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired
+	private PersonServices service;
+	// PersonServices service = new PersonServices();
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findByAll() {
+
+		return service.findAll();
+	}
+
+	@GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value = "id") String id) {
+
+		return service.findById(id);
+	}
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person) {
+		return service.create(person);
+	}
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+
+	@DeleteMapping(value = "{id}")
+	public void delete(@PathVariable(value = "id") String id) {
+
+		service.delete(id);
+	}
+}
