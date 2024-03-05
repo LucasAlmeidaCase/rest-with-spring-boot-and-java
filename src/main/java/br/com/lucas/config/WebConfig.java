d575ca59ec5445a7c8fdb46bcdb3480988c462aa
Lucas Almeida <dev2@case.inf.br>
2024-03-05 09:32:04 -0300

    Content Negotiation via HEADER Parameter

--- src/main/java/br/com/lucas/config/WebConfig.java
+++ src/main/java/br/com/lucas/config/WebConfig.java
@@ -16,10 +16,17 @@ public class WebConfig implements WebMvcConfigurer {
 
 		// Via QUERY PARAM. http://localhost:8080/api/person/v1?mediaType=xml
 
-		configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(true)
-				.useRegisteredExtensionsOnly(false).defaultContentType(MediaType.APPLICATION_JSON)
-				.mediaType("json", MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+		/*
+		 * configurer.favorParameter(true).parameterName("mediaType").ignoreAcceptHeader
+		 * (true) .useRegisteredExtensionsOnly(false).defaultContentType(MediaType.
+		 * APPLICATION_JSON) .mediaType("json",
+		 * MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML);
+		 */
+
+		// Via HEADER PARAM. http://localhost:8080/api/person/v1
+
+		configurer.favorParameter(false).ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
+				.defaultContentType(MediaType.APPLICATION_JSON).mediaType("json", MediaType.APPLICATION_JSON)
+				.mediaType("xml", MediaType.APPLICATION_XML);
 	}
 }
-
-
