5945c5551be935512fbbd9cf2b786117367086d4
Lucas Almeida <dev2@case.inf.br>
2024-03-12 10:50:27 -0300

    Validando a geração da Swagger UI Page com Testcontainers e Rest Assured

--- src/test/java/br/com/lucas/integrationtests/testcontainers/AbstractIntegrationTest.java
+++ src/test/java/br/com/lucas/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -14,7 +14,8 @@ import org.testcontainers.lifecycle.Startables;
 @ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
 public class AbstractIntegrationTest {
 
-	public class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+	static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
 		static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0.36");
 
 		private static void startContainers() {
