f287f34b55f8a8852c4af396bf364754e0223c32
Lucas Almeida <lucas@uaicontrole.com.br>
2024-04-25 10:15:15 -0300

    Ajustar os testes quebrados do endpoint de books

---
+++ src/test/java/br/com/erudio/integrationtests/vo/pagedmodels/PagedModelBook.java
@@ -0,0 +1,29 @@
+package br.com.erudio.integrationtests.vo.pagedmodels;
+
+import java.util.List;
+
+import br.com.erudio.integrationtests.vo.BookVO;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class PagedModelBook {
+
+	@XmlElement(name = "content")
+	private List<BookVO> content;
+
+	public PagedModelBook() {
+	}
+
+	public PagedModelBook(List<BookVO> content) {
+		this.content = content;
+	}
+
+	public List<BookVO> getContent() {
+		return content;
+	}
+
+	public void setContent(List<BookVO> content) {
+		this.content = content;
+	}
+}
